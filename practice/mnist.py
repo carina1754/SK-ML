6e6baa4e22e6b9e648b3d6ba15e74dddad745141
carina1754 <bulebed1754@gmail.com>
2020-09-29 23:24:10 +0900

    6일차

---
+++ practice/mnist.py
@@ -0,0 +1,123 @@
+# 파이썬 ≥3.5 필수
+import sys
+assert sys.version_info >= (3, 5)
+
+# 사이킷런 ≥0.20 필수
+import sklearn
+assert sklearn.__version__ >= "0.20"
+
+# 공통 모듈 임포트
+import numpy as np
+import os
+
+# 노트북 실행 결과를 동일하게 유지하기 위해
+np.random.seed(42)
+
+# 깔끔한 그래프 출력을 위해
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.rc('axes', labelsize=14)
+mpl.rc('xtick', labelsize=12)
+mpl.rc('ytick', labelsize=12)
+
+# 그림을 저장할 위치
+PROJECT_ROOT_DIR = "."
+CHAPTER_ID = "classification"
+IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID)
+os.makedirs(IMAGES_PATH, exist_ok=True)
+
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
+    print("그림 저장:", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
+
+from sklearn.datasets import fetch_openml
+mnist = fetch_openml('mnist_784', version=1)
+print(mnist.keys())
+
+X, y = mnist["data"], mnist["target"]
+print(X.shape)
+
+print(y.shape)
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+some_digit = X[0]
+some_digit_image = some_digit.reshape(28, 28)
+plt.imshow(some_digit_image, cmap=mpl.cm.binary)
+plt.axis("off")
+
+save_fig("some_digit_plot")
+#plt.show()
+
+y = y.astype(np.uint8)
+
+# 숫자 그림을 위한 추가 함수
+def plot_digits(instances, images_per_row=10, **options):
+    size = 28
+    images_per_row = min(len(instances), images_per_row)
+    images = [instance.reshape(size,size) for instance in instances]
+    n_rows = (len(instances) - 1) // images_per_row + 1
+    row_images = []
+    n_empty = n_rows * images_per_row - len(instances)
+    images.append(np.zeros((size, size * n_empty)))
+    for row in range(n_rows):
+        rimages = images[row * images_per_row : (row + 1) * images_per_row]
+        row_images.append(np.concatenate(rimages, axis=1))
+    image = np.concatenate(row_images, axis=0)
+    plt.imshow(image, cmap = mpl.cm.binary, **options)
+    plt.axis("off")
+
+plt.figure(figsize=(9,9))
+example_images = X[:100]
+plot_digits(example_images, images_per_row=10)
+save_fig("more_digits_plot")
+#plt.show()
+
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+
+y_train_5 = (y_train == 5)
+y_test_5 = (y_test == 5)
+
+from sklearn.linear_model import SGDClassifier
+
+sgd_clf = SGDClassifier(max_iter=1000, tol=1e-3, random_state=42)
+sgd_clf.fit(X_train, y_train_5)
+
+print(sgd_clf.predict([some_digit]))
+
+from sklearn.model_selection import cross_val_score
+print(cross_val_score(sgd_clf, X_train, y_train_5, cv=3, scoring="accuracy"))
+
+from sklearn.model_selection import StratifiedKFold
+from sklearn.base import clone
+
+# shuffle=False가 기본값이기 때문에 random_state를 삭제하던지 shuffle=True로 지정하라는 경고가 발생합니다.
+# 0.24버전부터는 에러가 발생할 예정이므로 향후 버전을 위해 shuffle=True을 지정합니다.
+skfolds = StratifiedKFold(n_splits=3, random_state=42, shuffle=True)
+
+for train_index, test_index in skfolds.split(X_train, y_train_5):
+    clone_clf = clone(sgd_clf)
+    X_train_folds = X_train[train_index]
+    y_train_folds = y_train_5[train_index]
+    X_test_fold = X_train[test_index]
+    y_test_fold = y_train_5[test_index]
+
+    clone_clf.fit(X_train_folds, y_train_folds)
+    y_pred = clone_clf.predict(X_test_fold)
+    n_correct = sum(y_pred == y_test_fold)
+    print(n_correct / len(y_pred))
+
+from sklearn.base import BaseEstimator
+class Never5Classifier(BaseEstimator):
+    def fit(self, X, y=None):
+        pass
+    def predict(self, X):
+        return np.zeros((len(X), 1), dtype=bool)
+
+never_5_clf = Never5Classifier()
+print(cross_val_score(never_5_clf, X_train, y_train_5, cv=3, scoring="accuracy"))
+
