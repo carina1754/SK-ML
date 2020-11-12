52c14439f2aa4e55c602fe57161c5b857fa2e261
carina1754 <bulebed1754@gmail.com>
2020-11-12 23:35:16 +0900

    202011121135

--- DL/keras-example.py
+++ DL/keras-example.py
@@ -55,9 +55,9 @@ y_valid, y_train = y_train_full[:5000], y_train_full[5000:]
 X_test = X_test / 255.
 
 class_names = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat","Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
-print(class_names[y_train[0]])
-print(X_valid.shape)
-print(X_test.shape)
+#print(class_names[y_train[0]])
+#print(X_valid.shape)
+#print(X_test.shape)
 
 n_rows = 4
 n_cols = 10
@@ -71,5 +71,77 @@ for row in range(n_rows):
         plt.title(class_names[y_train[index]], fontsize=12)
 plt.subplots_adjust(wspace=0.2, hspace=0.5)
 save_fig('fashion_mnist_plot', tight_layout=False)
-plt.show()
+#plt.show()
 #기본 이미지 출력
+
+model = keras.models.Sequential()
+model.add(keras.layers.Flatten(input_shape=[28, 28]))#입력층
+model.add(keras.layers.Dense(300, activation="relu"))#은닉층
+model.add(keras.layers.Dense(100, activation="relu"))#은닉층
+model.add(keras.layers.Dense(10, activation="softmax"))#출력층
+
+keras.backend.clear_session()
+np.random.seed(42)
+tf.random.set_seed(42)
+
+model.layers
+
+model.summary()
+
+hidden1 = model.layers[1]
+hidden1.name
+
+model.get_layer(hidden1.name) is hidden1
+
+weights, biases = hidden1.get_weights()
+weights
+
+biases
+
+biases.shape
+
+model.compile(loss="sparse_categorical_crossentropy",
+              optimizer="sgd",
+              metrics=["accuracy"])
+
+
+history = model.fit(X_train, y_train, epochs=30,
+                    validation_data=(X_valid, y_valid))
+
+history.params
+
+print(history.epoch)
+
+history.history.keys()
+
+import pandas as pd
+
+pd.DataFrame(history.history).plot(figsize=(8, 5))
+plt.grid(True)
+plt.gca().set_ylim(0, 1)
+save_fig("keras_learning_curves_plot")
+plt.show()
+
+model.evaluate(X_test, y_test)
+
+X_new = X_test[:3]
+y_proba = model.predict(X_new)
+y_proba.round(2)
+
+y_pred = model.predict_classes(X_new)
+y_pred
+
+np.array(class_names)[y_pred]
+
+y_new = y_test[:3]
+y_new
+
+plt.figure(figsize=(7.2, 2.4))
+for index, image in enumerate(X_new):
+    plt.subplot(1, 3, index + 1)
+    plt.imshow(image, cmap="binary", interpolation="nearest")
+    plt.axis('off')
+    plt.title(class_names[y_test[index]], fontsize=12)
+plt.subplots_adjust(wspace=0.2, hspace=0.5)
+save_fig('fashion_mnist_images_plot', tight_layout=False)
+plt.show()
