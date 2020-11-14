716a12ed32ec39c70cd761bb0f79e2e6d88e7eab
carina1754 <bulebed1754@gmail.com>
2020-11-14 23:43:23 +0900

    202011141143

---
+++ DL/regression-example.py
@@ -0,0 +1,76 @@
+# 파이썬 ≥3.5 필수
+import sys
+assert sys.version_info >= (3, 5)
+
+# 사이킷런 ≥0.20 필수
+import sklearn
+assert sklearn.__version__ >= "0.20"
+
+# 텐서플로 ≥2.0 필수
+import tensorflow as tf
+assert tf.__version__ >= "2.0"
+
+# 공통 모듈 임포트
+import numpy as np
+import os
+import tensorflow as tf
+from tensorflow import keras
+# 노트북 실행 결과를 동일하게 유지하기 위해
+np.random.seed(42)
+
+# 깔끔한 그래프 출력을 위해
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.rc('axes', labelsize=14)
+mpl.rc('xtick', labelsize=12)
+mpl.rc('ytick', labelsize=12)
+
+# 그림을 저장할 위치
+PROJECT_ROOT_DIR = "."
+CHAPTER_ID = "ann"
+IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID)
+os.makedirs(IMAGES_PATH, exist_ok=True)
+
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
+    print("그림 저장:", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
+
+# 불필요한 경고를 무시합니다 (사이파이 이슈 #5998 참조)
+import warnings
+warnings.filterwarnings(action="ignore", message="^internal gelsd")
+
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+housing = fetch_california_housing()
+
+X_train_full, X_test, y_train_full, y_test = train_test_split(housing.data, housing.target, random_state=42)
+X_train, X_valid, y_train, y_valid = train_test_split(X_train_full, y_train_full, random_state=42)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_valid = scaler.transform(X_valid)
+X_test = scaler.transform(X_test)
+
+np.random.seed(42)
+tf.random.set_seed(42)
+
+model = keras.models.Sequential([
+    keras.layers.Dense(30, activation="relu", input_shape=X_train.shape[1:]),
+    keras.layers.Dense(1)
+])
+model.compile(loss="mean_squared_error", optimizer=keras.optimizers.SGD(lr=1e-3))
+history = model.fit(X_train, y_train, epochs=20, validation_data=(X_valid, y_valid))
+mse_test = model.evaluate(X_test, y_test)
+X_new = X_test[:3]
+y_pred = model.predict(X_new)
+import pandas as pd
+plt.plot(pd.DataFrame(history.history))
+plt.grid(True)
+plt.gca().set_ylim(0, 1)
+plt.show()
+
